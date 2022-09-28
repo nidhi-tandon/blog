fc53c1828d1f8a405f11b0a212a51542c60b5a4e
Nidhi Tandon <nidhitandon08@gmail.com>
2022-09-28 16:26:34 +0530

    feat: add responsiveness

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap"
           rel="stylesheet"
